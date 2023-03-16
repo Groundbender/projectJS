91b21f29da65688895920cc20bb29da85a622f72
Damir <damir24may@mail.ru>
2023-03-16 23:13:52 +0300

    callback modal

--- src/modules/callbackModal.js
+++ src/modules/callbackModal.js
@@ -1,5 +1,17 @@
 const callbackModal = () => {
-  console.log("callbackModal");
+  const callbackBtn = document.getElementsByClassName("btn")[1];
+  const callbackModal = document.querySelector(".header-modal");
+  const callbackModalCLose = document.querySelector(".header-modal__close");
+  const overlay = document.querySelector(".overlay");
+
+  callbackBtn.addEventListener("click", () => {
+    overlay.style.display = "block";
+    callbackModal.style.display = "block";
+  });
+  callbackModalCLose.addEventListener("click", () => {
+    overlay.style.display = "none";
+    callbackModal.style.display = "none";
+  });
 };
 
 export default callbackModal;
