8bb8fbe10011f91913dd550991fbcd567fddfbfe
Damir <damir24may@mail.ru>
2023-03-17 18:57:19 +0300

    validation

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,8 +1,19 @@
 const validation = () => {
-  console.log("valid");
+  // console.log("valid");
   const forms = document.querySelectorAll("form");
 
   console.log(forms);
+  forms.forEach((form) => {
+    let inputName = form.querySelector("input[name='fio']");
+    let inputTel = form.querySelector("input[name='phone']");
+
+    inputName.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-яА-Яa-zA-Z-\s]/g, "");
+    });
+    inputTel.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^\+\d]/g, "");
+    });
+  });
 };
 
 export default validation;
