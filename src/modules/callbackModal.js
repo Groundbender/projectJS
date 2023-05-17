d7df0409b5c3871aca37293fa4bbd8678508a2bb
Damir <damir24may@mail.ru>
2023-05-17 17:20:27 +0300

    improved modal callback window

--- src/modules/callbackModal.js
+++ src/modules/callbackModal.js
@@ -6,12 +6,21 @@ const callbackModal = () => {
   const callbackModalCLose = document.querySelector(".header-modal__close");
   const overlay = document.querySelector(".overlay");
 
-  callbackBtn.addEventListener("click", () => {
-    overlay.style.display = "block";
-    callbackModal.style.display = "block";
+  const blockScroll = () => {
     let scrollWidth = window.innerWidth - document.documentElement.clientWidth;
     document.body.style.paddingRight = `${scrollWidth}px`;
     document.body.style.overflow = "hidden";
+  };
+
+  const unBlockScroll = () => {
+    document.body.style.paddingRight = `0px`;
+    document.body.style.overflow = "";
+  };
+
+  const showModal = () => {
+    // clearTimeout(modalTimerId);
+    overlay.style.display = "block";
+    callbackModal.style.display = "block";
     if (screen.width > 768) {
       animate({
         duration: 500,
@@ -23,13 +32,47 @@ const callbackModal = () => {
         },
       });
     }
-  });
-  callbackModalCLose.addEventListener("click", () => {
+  };
+
+  const closeModal = () => {
     overlay.style.display = "none";
     callbackModal.style.display = "none";
-    document.body.style.paddingRight = `0px`;
-    document.body.style.overflow = "";
+  };
+
+  const showModalByScroll = () => {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      showModal();
+      blockScroll();
+
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  };
+
+  callbackBtn.addEventListener("click", () => {
+    showModal();
+    blockScroll();
   });
+  callbackModalCLose.addEventListener("click", () => {
+    closeModal();
+    unBlockScroll();
+  });
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      closeModal();
+      unBlockScroll();
+    }
+  });
+
+  // const modalTimerId = setTimeout(() => {
+  //   showModal();
+  //   blockScroll();
+  // }, 5000);
 };
 
 export default callbackModal;
